32fdf664b71257081a9e61465f3db15fed3abdd5
Sandip Kundu <sandipkundu06.sk@gmail.com>
2026-08-30 19:47:33 +0530

    Update application version to v2 and enhance deployment configuration with rolling updates and health probes

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return {
-        "message": "Hello from Kubernetes!",
+        "message": "Hello from Kubernetes! v2",
         "hostname": socket.gethostname()
     }
 
